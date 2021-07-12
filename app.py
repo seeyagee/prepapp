a2e229f3d79c06c21ef5282140f778093f927731
merionum <vadim0006@gmail.com>
2021-07-12 12:14:40 +0300

    Added files for deployment

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 
-st.title('Предложные конструкции в русском языке')
-st.write('')
-df = pd.read_csv('static/prep_phrases_gold.csv.zip', compression='zip')
-df = df[['phrase', 'label']]
+# st.title('Предложные конструкции в русском языке')
+# st.write('')
+# df = pd.read_csv('static/prep_phrases_gold.csv.zip', compression='zip')
+# df = df[['phrase', 'label']]
 
-# st.write("## Data", df)
+# # st.write("## Data", df)
 
 # genre = st.radio(
 #     "What's your favorite movie genre",
@@ -18,8 +19,16 @@ df = df[['phrase', 'label']]
 #     st.write("You didn't select comedy.")
 
 
-options = st.multiselect(
-     'What are your favorite colors',
-    ['Blue' for _ in range(100)]+['Yellow']+ ['Red'])
+# options = st.multiselect(
+#      'What are your favorite colors',
+#     ['Blue' for _ in range(100)]+['Yellow']+ ['Red']	)
 
-st.write('You selected:', options)
+# st.write('You selected:', options)
+
+
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
+
+    chart_data
