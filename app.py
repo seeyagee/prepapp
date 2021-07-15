6cf0e32538a9427cc30f4a35da1a0945ccf763b8
merionum <vadim0006@gmail.com>
2021-07-15 15:13:45 +0300

    Добавлена форма поиска по таблице и скачивание файла

--- app.py
+++ app.py
@@ -17,69 +17,77 @@ prep = st.selectbox(
     'Выберите предлог, чтобы узнать больше',
     ['']+all_preps)
 
-if not prep:
-    st.stop()
-
-prep_df = synt_df[synt_df.prep == prep]
-prep_labels = sorted(prep_df.label.unique())
+if prep:
+    prep_df = synt_df[synt_df.prep == prep]
+    prep_labels = sorted(prep_df.label.unique())
 
 
-st.markdown(f'#### Значения предлога *{prep.upper()}*:')
-for l in prep_labels:
-    with st.beta_expander(f'{l}'):
+    st.markdown(f'#### Значения предлога *{prep.upper()}*:')
+    for l in prep_labels:
+        with st.beta_expander(f'{l}'):
 
-        short_df = synt_df[(synt_df.prep == prep) & (synt_df.label == l)]
-        definitions = short_df['definition'].iloc[0].split(';')
-        for d in definitions:
-            st.write(f"""
-            - {d}
-            """)
-        st.write("###\n## Примеры: ")
-        for _, row in short_df.iterrows():
+            short_df = synt_df[(synt_df.prep == prep) & (synt_df.label == l)]
+            definitions = short_df.definition.iloc[0].split(';')
+            
+            for d in definitions:
+                st.write(f'- {d}')
+            st.write('###\n## Примеры: ')
+            
+            for _, row in short_df.iterrows():
+                st.write(f'### {row.case} падеж:')
+            
+                for ex in row.examples.split(','):
+                    st.write(f'*{ex.strip()}*\n')
 
-            st.write(f"### {row['case']} падеж:")
-            for ex in row['examples'].split(','):
-                st.write(f"*{ex.strip()}*\n")
+
+    label = st.selectbox(
+        'Выберите значение',
+        [''] + prep_labels) 
+
+    if label:
+        pl_df = phras_df[(phras_df.label == label.lower()) & (phras_df.prep == prep)]
+        hosts = pl_df.host_lemma.value_counts().head(10)
+        dependants = pl_df.dependant_lemma.value_counts().head(10)
+        
+        if st.checkbox('Показать хозяев'):
+            hosts_fig = utils.show_hbar(
+                hosts.index,
+                hosts.values,
+                title='Относительная частота хозяев синтаксемы')
+            st.write(hosts_fig)
+
+        if st.checkbox('Показать слуг'):
+            deps_fig = utils.show_hbar(
+                dependants.index,
+                dependants.values,
+                title='Относительная частота слуг синтаксемы')
+            st.write(deps_fig)
 
 
-label = st.selectbox(
-    'Выберите значение',
-    [''] + prep_labels) 
+with st.beta_expander('Сформировать запрос в банк предложных конструкций:'):
+    query = {}
 
-if label:
-    pl_df = phras_df[(phras_df.label == label.lower()) & (phras_df.prep == prep)]
-    hosts = pl_df.host_lemma.value_counts().head(10)
-    dependants = pl_df.dependant_lemma.value_counts().head(10)
+    query_values = (
+        'prep', 'host_lemma', 'dependant_lemma',
+        'dependant_case', 'host_pos', 'dependant_pos')
+
+    for col in query_values:
+
+        query[col] = st.multiselect(
+                    col,
+                    phras_df[col].unique().tolist())
     
-    if st.checkbox('Показать основных хозяев'):
-        hosts_fig = utils.show_hbar(
-            hosts.index,
-            hosts.values,
-            title='Относительная частота хозяев синтаксемы')
-        st.write(hosts_fig)
+    if st.checkbox('Submit'):
 
-    if st.checkbox('Показать основных слуг'):
-        deps_fig = utils.show_hbar(
-            dependants.index,
-            dependants.values,
-            title='Относительная частота слуг синтаксемы')
-        st.write(deps_fig)
+        query  = ' and '.join(
+            f'{key} in {val}' for key, val in query.items()
+            if len(val))
+        out_df = phras_df.query(query) if query else phras_df
+        st.write(out_df)
 
-
-# fig, ax = plt.subplots()
-
-# # Example data
-# people = ('Tom', 'Dick', 'Harry', 'Slim', 'Jim')
-# y_pos = np.arange(len(people))
-# performance = 3 + 10 * np.random.rand(len(people))
-# error = np.random.rand(len(people))
-
-# ax.barh(y_pos, performance, xerr=error, align='center')
-# ax.set_yticks(y_pos)
-# ax.set_yticklabels(people)
-# ax.invert_yaxis()  # labels read top-to-bottom
-# ax.set_xlabel('Performance')
-# ax.set_title('How fast do you want to go today?')
-
-# st.write(fig)
-
+        # if st.button('Скачать таблицу в формате CSV'):
+        tmp_link = utils.get_table_download_button(
+            out_df,
+            'prep_phrases_query.csv',
+            'Cкачать')
+        st.markdown(tmp_link, unsafe_allow_html=True)
--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import base64
+import re
+import uuid
 
 
 def show_hbar(names, values, title, kind='relative'):
@@ -14,7 +17,44 @@ def show_hbar(names, values, title, kind='relative'):
     ax.barh(y_pos, values, align='center')
     ax.set_yticks(y_pos)
     ax.set_yticklabels(names)
-    ax.invert_yaxis()  # labels read top-to-bottom
+    ax.invert_yaxis()
     ax.set_title(title)
 
     return fig
+
+def get_table_download_button(df, filename, linktext):
+
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" id="b0" download="{filename}">{linktext}</a><br></br>'
+
+    custom_css = f""" 
+        <style>
+            #b0 {{
+                display: inline-flex;
+                align-items: center;
+                justify-content: center;
+                background-color: rgb(255, 255, 255);
+                color: rgb(246, 51, 102);
+                padding: .25rem .75rem;
+                position: relative;
+                text-decoration: none;
+                border-radius: 4px;
+                border-width: 1px;
+                border-style: solid;
+                border-color: rgb(230, 234, 241);
+                border-image: initial;
+            }} 
+            #b0:hover {{
+                border-color: rgb(246, 51, 102);
+                color: rgb(246, 51, 102);
+            }}
+            #b0:active {{
+                box-shadow: none;
+                background-color: rgb(246, 51, 102);
+                color: white;
+                }}
+        </style> """
+
+
+    return custom_css + href
